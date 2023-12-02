ad220c13940dca405795994bf39202ec969392b2
webdevsalmann <webdevsalman7869@gmail.com>
2023-12-02 23:14:54 +0530

    Update scan qr code functionality

--- src/app/(pages)/menu/CheckoutDetails.jsx
+++ src/app/(pages)/menu/CheckoutDetails.jsx
@@ -5,8 +5,14 @@ import toast from "react-hot-toast";
 
 
 export default function CheckoutDetails() {
-    const [fTableNo, setFTableNo] = useState("");
-    const handleOnChange = (e) => { setFTableNo(e.target.value); }
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let [tableValue, setTableValue] = useState(urlParams.get('table'));
+    // console.log(tableValue)
+    const [fTableNo, setFTableNo] = useState(tableValue);
+    const handleOnChange = (e) => {
+        setTableValue(e.target.value);
+    }
     // Calculate Total Amount
     const { orders, getItemById } = useData();
     const totalPrice = orders.map(item => {
@@ -19,13 +25,13 @@ export default function CheckoutDetails() {
     const botToken = '6756282629:AAF37Y6_3DZg2MzkPg6-CceUza3tGFyYktM';
     const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
     const chatId = '@wds_coffee_broadcast';
-    const message = `Table No: ${fTableNo}\n\nOrdered:\n${orders.map(order => `${order.itemId} = ${order.quantity}`).join('\n')}\n\nTotal Amount: $${total}`;
+    const message = `Table No: ${tableValue}\n\nOrdered:\n${orders.map(order => `${order.itemId} = ${order.quantity}`).join('\n')}\n\nTotal Amount: $${total}`;
 
     const params = { chat_id: chatId, text: message, };
 
     // Handle Sending Message to Telegram Channel
+    const tableNoConditions = tableValue.trim() === "";
     const sendMessage = async () => {
-        const tableNoConditions = fTableNo.trim() === "";
         if (tableNoConditions) {
             return toast.error("Please Enter Table Number")
         } else {
@@ -41,20 +47,17 @@ export default function CheckoutDetails() {
 
     return (
         <div className="p-4 set-bg-dark rounded-md">
-            {/* Count total */}
-            <div className="">
-                <span>Total Amount</span> <span className="font-bold clr-p">$ {total && total}</span>
-            </div>
+            <span>Total Amount</span> <span className="font-bold clr-p">$ {total && total}</span>
 
             <div className="mt-4">
                 <label className="flex items-center gap-2" htmlFor="fTableNo">
                     <span>Table No: </span>
-                    <input className="set-bg-light max-w-[5rem]" type="text" name="fTableNo" value={fTableNo} onChange={handleOnChange} placeholder="No" />
+                    <input className="set-bg-light max-w-[5rem]" type="text" name="fTableNo" value={tableValue} onChange={handleOnChange} placeholder="No" />
                 </label>
             </div>
 
             <div className="mt-6">
-                <button className="btn-outline" onClick={sendMessage}>Place Order</button>
+                <button className={`btn-outline ${tableNoConditions ? "cursor-not-allowed set-text-balance" : "cursor-pointer set-text"}`} onClick={sendMessage}>Place Order</button>
             </div>
         </div>
     )
